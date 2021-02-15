3ca5d015cda7888ce769a329f27c5aed4ee61b80
AxelJurczuk <61692326+AxelJurczuk@users.noreply.github.com>
2021-02-15 16:22:00 -0300

    adapter

---
+++ app/src/main/java/com/example/android/dogsgallery/ui/overview/DogsAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.android.dogsgallery.ui.overview
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.dogsgallery.R
+import com.squareup.picasso.Picasso
+
+class DogsAdapter(private val context: Context): RecyclerView.Adapter<DogsAdapter.DogsViewHolder>() {
+
+    var dogsList:List<String> = emptyList()
+
+    class DogsViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+        val dogImage:ImageView = view.findViewById(R.id.iv_dog)
+        val dogName:TextView = view.findViewById(R.id.tv_dog_name)
+        val dogDescription:TextView = view.findViewById(R.id.tv_dog_description)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogsViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list, parent,false)
+        return DogsViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: DogsViewHolder, position: Int) {
+        val item = dogsList[position]
+        holder.dogName.text = "Dog ${position+1}"
+        holder.dogDescription.text = "Really cute dog"
+        Picasso.get()
+            .load(item)
+            .fit()
+            .centerCrop()
+            .into(holder.dogImage)
+    }
+
+    override fun getItemCount(): Int {
+        return dogsList.size
+    }
+
+}
--- app/src/main/java/com/example/android/dogsgallery/ui/overview/OverViewFragment.kt
+++ app/src/main/java/com/example/android/dogsgallery/ui/overview/OverViewFragment.kt
@@ -9,21 +9,29 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.android.dogsgallery.R
 import com.example.android.dogsgallery.data.Result
+import com.example.android.dogsgallery.databinding.FragmentOverViewBinding
 
 
 class OverViewFragment : Fragment() {
 
+    private var _binding: FragmentOverViewBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: DogsViewModel by viewModels()
+    lateinit var adapter: DogsAdapter
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
+        _binding = FragmentOverViewBinding.inflate(inflater, container, false)
+        val view = binding.root
         val dogsObserver = Observer<Result>{
             when(it) {
                 is Result.Success -> {
-                    Log.i("dogsList", it.list.toString())
-                    Toast.makeText(requireContext(), "fragment working", Toast.LENGTH_SHORT).show()
+                    adapter.dogsList = it.list
+                    adapter.notifyDataSetChanged()
                 }
                 is Result.Failure -> Toast.makeText(requireContext(),
                         "Something went wrong",
@@ -33,7 +41,22 @@ class OverViewFragment : Fragment() {
 
         viewModel.getListLiveData().observe(viewLifecycleOwner,dogsObserver)
 
-        return inflater.inflate(R.layout.fragment_over_view, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter= DogsAdapter(requireContext())
+        val recyclerView = binding.recyclerView
+        recyclerView.adapter= adapter
+        recyclerView.layoutManager= GridLayoutManager(requireContext(),2)
+        recyclerView.setHasFixedSize(true)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
