847b0937f89479bcbd6db43582ba6bd30a23dc55
AxelJurczuk <61692326+AxelJurczuk@users.noreply.github.com>
2021-02-15 11:22:50 -0300

    overview

---
+++ app/src/main/java/com/example/android/dogsgallery/ui/overview/OverViewFragment.kt
@@ -0,0 +1,36 @@
+package com.example.android.dogsgallery.ui.overview
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.example.android.dogsgallery.R
+import com.example.android.dogsgallery.data.Result
+
+
+class OverViewFragment : Fragment() {
+
+    val viewModel: DogsViewModel by viewModels()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val dogsObserver = Observer<Result>{
+            when(it) {
+                is Result.Success-> Log.i("dogsList", it.list.toString())
+                is Result.Failure -> Toast.makeText(requireContext(),
+                        "Something went wrong",
+                        Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        viewModel.getListLiveData().observe(viewLifecycleOwner,dogsObserver)
+
+        return inflater.inflate(R.layout.fragment_over_view, container, false)
+    }
+
+}
